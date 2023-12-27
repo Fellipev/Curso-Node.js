6a06237acd9696891729434bd71a4f97cbb5f06e
Fellipev <fellipe2reis@gmail.com>
2023-12-27 08:08:02 -0300

    Aula 24 - Save/insert no banco de dados

--- Curso Node/aula18.2/index.js
+++ Curso Node/aula18.2/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
+const Post = require('./models/Post')
 
 app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
@@ -13,12 +14,25 @@ app.get('/cad', (req, res) =>{
     res.render('formulario')
 })
 
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
 app.post('/addPostagem', (req, res) => {
     //como pegar os campos enviados pelo form
-    let titulo = req.body.titulo
-    let conteudo = req.body.conteudo
-    
-    res.send(`Titulo: ${titulo}<br>Conteudo: ${conteudo}`)
+        let titulo = req.body.titulo
+        let conteudo = req.body.conteudo
+        // res.send(`Titulo: ${titulo}<br>Conteudo: ${conteudo}`)
+
+    //Criação do post para salvar no banco
+    Post.create({
+        titulo: titulo,
+        conteudo: conteudo
+    }).then(() => {
+        res.redirect('/')
+    }).catch((error) => {
+        res.send("Houve um erro: " + error)
+    })
 })
 
 app.listen(8081, () => {
